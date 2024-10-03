7984be7122cf519002e488e9e09e8883547778a3
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-10-02 22:39:38 -0400

    fixed logic error in data upload

--- backend/django-api/transaction/view.py
+++ backend/django-api/transaction/view.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from RattmWeb.firebase import db
-
+from dotenv import load_dotenv
 import csv
 import json
+import os
 
 def make_json(csvFilePath, jsonFilePath):
-    
     # create a dictionary
     data = {}
     
@@ -31,19 +31,16 @@ def make_json(csvFilePath, jsonFilePath):
 
 # Put data in Firestore
 def upload_data_to_firestore(request):
-    data = {
-        "Transaction ID": "TXN001",
-        "Client ID": "CL001",
-        "Date": "2024-09-01",
-        "Company Name": "EcoMarket",
-        "Location - Latitude": 40.7128,
-        "Location - Longitude": -74.006,
-        "Transaction Amount": 45.5
-    }
+    load_dotenv()
+    mock_json_path = os.getenv('MOCK_JSON_PATH')
+    with open(mock_json_path, 'r') as file:
+        data = json.load(file)
 
     try:
         # don't need to first create the table, can just start adding and the table will be created
-        db.collection('transactions').add(data) 
+        # data is a dictionary with {key - transactionid : value - {dict of all transaction info}}
+        for transaction_id in data:
+            db.collection('transactions').add(data[transaction_id])
         return JsonResponse({'message': 'Data uploaded successfully'}, status=200)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
@@ -68,4 +65,5 @@ def get_data_from_firestore(request):
 
 # Fake function rn to give esg rating for company
 def esg_rating(transaction_dict):
+    print(transaction_dict)
     return len(transaction_dict["Company Name"]) // 3
