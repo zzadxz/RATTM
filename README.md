2e2055a8c4caade9f387c2c8f12848aa9f039d1d
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-10-02 21:36:10 -0400

    requirements

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # RATTM
 RATTM FTL project
 
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
+      allow read, write: if false;
+    }
+  }
+}
+
 ## backend
 **File Structure**\
 django-api/\
--- backend/django-api/transaction/view.py
+++ backend/django-api/transaction/view.py
@@ -3,6 +3,32 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from RattmWeb.firebase import db
 
+import csv
+import json
+
+def make_json(csvFilePath, jsonFilePath):
+    
+    # create a dictionary
+    data = {}
+    
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+        
+        # Convert each row into a dictionary 
+        # and add it to data
+        for rows in csvReader:
+            
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = rows['No']
+            data[key] = rows
+
+    # Open a json writer, and use the json.dumps() 
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
 # Put data in Firestore
 def upload_data_to_firestore(request):
     data = {
