eac01e48eeaf6403d4d832bf9e7a88ca4e89cfd4
YoyoLiuuu <yoyoyimeng.liu@mail.utoronto.ca>
2024-10-21 18:59:49 -0400

    fix identifier issues

--- backend/RattmWeb/settings.py
+++ backend/RattmWeb/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-l2s_1d%agc%yanv%53z08tet1xctbu$j@-u&qo^_*ye3zlr9-l'
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
