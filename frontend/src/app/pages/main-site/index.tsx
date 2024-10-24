34bcc0e6633c8e1ab164e3923465d99e633ca1a8
Kiarash Sotoudeh <kiarashsotoudeh@gmail.com>
2024-10-23 23:09:45 -0400

    Update welcome message and subheading in MainSite component

--- frontend/src/app/pages/main-site/index.tsx
+++ frontend/src/app/pages/main-site/index.tsx
@@ -88,7 +88,7 @@ const MainSite: React.FC = () => {
 
   return (
     <MainContainer>
-      <MainHeading>Welcome to RATTM&apos;s CO2 Calculator!</MainHeading>
+      <MainHeading>Welcome ALL HAHAHHAHAHA &apos;s CO2 Calculator!</MainHeading>
       <SubHeading>View Your Recent Transactions</SubHeading>
 
       {/* if there is an error, show it here */}
