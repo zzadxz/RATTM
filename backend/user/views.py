2252e94f816bef67fb2b773111c13aa195f681b0
YoyoLiuuu <yoyoyimeng.liu@mail.utoronto.ca>
2024-11-01 23:57:37 -0400

    map implementation

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from utils.firebase import db
+from django.http import JsonResponse
 
 # create 
-# documentation: https://docs.djangoproject.com/en/5.1/topics/class-based-views/
-# class-based view 
-# def create_user(request):
-    
+
+def get_all_transactions(user_id):
+    # Get all transactions from Firestore
+    try:
+        transactions_ref = db.collection('transactions')
+        docs = transactions_ref.stream()
+
+        transactions = []
+        # a document is a row in the table (transaction obj)
+        for doc in docs:
+            transaction = doc.to_dict()
+            if transaction["customerID"] == user_id:
+                transactions.append(transaction)
+
+        return transactions
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+def 
