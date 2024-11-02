8c48f565d71735f1bd5243012a4d7b8e0e889827
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-11-01 23:24:07 -0400

    Update page.tsx

--- frontend/src/app/main-site/auth/SignInWithGoogle/page.tsx
+++ frontend/src/app/main-site/auth/SignInWithGoogle/page.tsx
@@ -5,7 +5,6 @@ import React, { useEffect, useState } from "react";
 import { signInWithGoogle } from "@/app/firebase/authService";
 import { useRouter } from "next/navigation";
 import { auth } from "@/app/firebase/firebaseConfig";
-import { send } from "process";
 
 function SendEmail(userEmail: string) {
 
