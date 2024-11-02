7ce632c623d86e807568f22234cc602c87c5eff9
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-11-01 23:26:03 -0400

    delete user folder

--- backend/user/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# create 
-# documentation: https://docs.djangoproject.com/en/5.1/topics/class-based-views/
-# class-based view 
-# def create_user(request):
-    
