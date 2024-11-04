c60a5d5f81fb05617a686f358865f90ac584fa4e
Chong <chong.wan@mail.utoronto.ca>
2024-11-03 23:29:22 -0500

    added the map function to return the user's transaction company name, transaction location, and transaction merchant's environmental score percentile

--- backend/static_file/map.py
+++ backend/static_file/map.py
@@ -1,23 +1,56 @@
 # get latitude and longitude of transactions of user
 from utils.firebase import db
 from django.http import JsonResponse
+from login.use_cases import get_table_from_firebase
+from env_impact_history import get_company_env_score, get_closest_match, company_tier
 
-def get_user_all_locations(user_id):
+# def get_user_all_locations(user_id):
+#     try:
+#         user = db.collection('Users').document(user_id)
+
+#         if not user:
+#             return JsonResponse({'error': 'User not found'}, status=404)
+        
+#         user_data = user.to_dict()
+#         user_transactions = user_data["transactions"]
+#         locations = []
+        
+#         for transaction in user_transactions: 
+#             curr_location = (transaction['latitude'], transaction['longitude'])
+#             locations.append(curr_location)
+
+#         return locations
+        
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=500)
+    
+    
+def get_user_all_locations_and_company(user_id):
     try:
-        user = db.collection('users').document(user_id)
+        user = db.collection('Users').document(user_id)
 
         if not user:
             return JsonResponse({'error': 'User not found'}, status=404)
         
-        user_data = user.to_dict()
-        user_transactions = user_data["transactions"]
-        locations = []
+        # This line will just get one user's data from firebase
+        User_data = db.collection('Users').document(user_id).get().to_dict()
+        user_transactions = User_data["transactions"]
+        
+        # This line will get all the ESG scores from firebase
+        ESG_scores = get_table_from_firebase('esg')
+        
+        map_data = []
         
         for transaction in user_transactions: 
-            curr_location = (transaction['latitude'], transaction['longitude'])
-            locations.append(curr_location)
+            company_name = get_closest_match(transaction['merchant_name'], ESG_scores.getkeys())
+            transact_map_data = {
+                "location": (transaction['latitude'], transaction['longitude']),
+                "merchant_name": transaction['merchant_name'],
+                "merchant_percentile": company_tier(ESG_scores[company_name]['environment_score']),
+            }
+            map_data.append(transact_map_data)
 
-        return locations
+        return map_data
         
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
