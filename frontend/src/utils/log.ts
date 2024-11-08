8c8d9935d240f5eef32c8fd6c26220c38dda1933
Kiarash Sotoudeh <kiarashsotoudeh@gmail.com>
2024-11-08 00:33:00 -0500

    Improve tests for MainSite component and mock Firebase auth; update logging function to use console.log in development mode

--- frontend/src/utils/log.ts
+++ frontend/src/utils/log.ts
@@ -2,6 +2,6 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 export const log = (...args: unknown[]) => {
   if (isDevelopment) {
-    log(...args);
+    console.log(...args);
   }
 };
