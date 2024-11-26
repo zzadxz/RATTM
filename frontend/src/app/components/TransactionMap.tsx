7662de54ad85276760adbbf67364c2b60a32ea99
Gabriel Thompson <gabrielezrathompson@gmail.com>
2024-11-26 03:19:55 -0500

    Made map pull & display data points; made API accessed from .env.local

--- frontend/src/app/components/TransactionMap.tsx
+++ frontend/src/app/components/TransactionMap.tsx
@@ -1,40 +1,91 @@
 "use client";
 
 import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useEffect, useRef } from "react";
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZ2FicmllbGV6cmF0aG9tcHNvbiIsImEiOiJjbTJ6OWpwcTQwOTh2MmxvZ3BpaW5wajhkIn0.BFA_fvxSDOBkZkAdXjnxiQ";
+const colorMap: { [key: number]: { textColor: string; description: string } } =
+  {
+    1: { textColor: "red", description: "Awful" },
+    2: { textColor: "orange", description: "Pretty Bad" },
+    3: { textColor: "gold", description: "Decent" },
+    4: { textColor: "green", description: "Great" },
+  };
+
+interface MapPoint {
+  location: Array<number>;
+  merchant_name: string;
+  merchant_percentile: number;
+}
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY || "";
 
 const TransactionMap: React.FC = () => {
-  const mapContainer = useRef(null);
+  const mapContainer = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    // Initialize the Mapbox map
-    const map = new mapboxgl.Map({
-      container: mapContainer.current || "",
-      style: "mapbox://styles/mapbox/streets-v11", 
-      center: [-74.5, 40], 
-      zoom: 9, 
-    });
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://rattm-f300025e7172.herokuapp.com/map/get_map_data/",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
 
-    return () => map.remove(); 
+      const map = new mapboxgl.Map({
+        container: mapContainer.current || "",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-79.390889, 43.667331],
+        zoom: 3,
+      });
+
+      data.forEach(
+        ({ location, merchant_name, merchant_percentile }: MapPoint) => {
+          const [latitude, longitude] = location;
+          const { textColor, description } = colorMap[merchant_percentile];
+          new mapboxgl.Marker()
+            .setLngLat([longitude, latitude])
+            .setPopup(
+              new mapboxgl.Popup({ offset: 25 }).setHTML(`
+                <h3 className='text-3xl' style='text-align: center; font-weight: bold;'>
+                  ${merchant_name}
+                </h3>
+                <p className="text-lg">
+                  Rating:
+                  <span style='color: ${textColor}'>${description}</span>
+                </p>
+              `)
+            )
+            .addTo(map);
+        }
+      );
+
+      return () => map.remove();
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div
-      ref={mapContainer}
-      className="map-container"
-      style={{
-        margin: "auto",
-        width: "80vw",
-        height: "69vh",
-        minHeight: "400px",
-        maxHeight: "100%",
-        borderRadius: "20px", 
-        overflow: "hidden",
-      }}
-    />
+    <>
+      <div
+        ref={mapContainer}
+        className="map-container"
+        style={{
+          margin: "auto",
+          width: "80vw",
+          height: "69vh",
+          minHeight: "400px",
+          maxHeight: "100%",
+          borderRadius: "20px",
+          overflow: "hidden",
+        }}
+      />
+    </>
   );
 };
 
--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,11 +1,7 @@
-import { Metadata } from "next";
-import TransactionMap from "@/app/components/TransactionMap";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Next.js Calender | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Calender page for TailAdmin  Tailwind CSS Admin Dashboard Template",
-};
+import TransactionMap from "@/app/components/TransactionMap";
+import React from "react";
 
 const Map = () => {
   return (
