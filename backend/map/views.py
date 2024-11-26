a49aaece02c1012afb11518c298b5575acd9a0ee
YoyoLiuuu <yoyoyimeng.liu@mail.utoronto.ca>
2024-11-26 17:24:31 -0500

    refactor view complete

--- backend/map/views.py
+++ backend/map/views.py
@@ -6,12 +6,22 @@ from .use_cases import (
     MapUseCase
 )
 
-# get map data
-def get_map_data(request):
+class MapView(): 
     """
-    According to the user id, user_map object is created.
-    User map location information is processed and returned.
+    This class is the view for the map tab on the website. 
+    Methods are used when getting useful purchase information for the map display, given a user. 
     """
-    user_id = request.session.get("user_id")  or '0'
-    user_map = MapUseCase(user_id)
-    return JsonResponse(user_map.get_user_all_locations_and_company(), safe=False)
+
+    def __init__(self):
+        # initialize the view without setting user information
+        self.user_id = None 
+    
+    def get_map_data(self, request):
+        """
+        According to the user id, user_map object is created.
+        User map location information is processed and returned.
+        """
+        # set user id according to the request 
+        self.user_id = request.request.session.get("user_id")  or '0'
+        user_map = MapUseCase(self.user_id)
+        return JsonResponse(user_map.get_user_all_locations_and_company(), safe=False)
