46e60614302ba08e9719c8af32bca533b16de48e
Jennifer Chiou <jennifer.r.chiou@gmail.com>
2024-11-27 20:01:09 -0500

    actually fixed login error this time (classes messed things up)

--- backend/login/views.py
+++ backend/login/views.py
@@ -1,12 +1,14 @@
+import json
 from rest_framework.response import Response
-from django.http import HttpResponseNotAllowed
+from django.http import JsonResponse, HttpResponseNotAllowed
 from .abstract_use_case import AbstractLoginUseCase
-
+from django.views.decorators.csrf import csrf_exempt
 
 class LoginView:
     def __init__(self, login_use_case: AbstractLoginUseCase):
         self.login_use_case = login_use_case
 
+    @csrf_exempt
     def get_user_email_from_frontend(self, request):
         """
         Endpoint: /login/get_email
@@ -15,6 +17,9 @@ class LoginView:
         if request.method != "POST":
             return HttpResponseNotAllowed(["POST"])
         
-        user_id = self.login_use_case.match_email_to_id(request.data)
+        body = json.loads(request.body)
+        user_email = body.get("userEmail")
+        user_id = self.login_use_case.match_email_to_id(user_email)
         request.session["user_id"] = user_id
-        return Response({"message": f"Got user's email {request.data}", "data": user_id})
+        print(user_id)
+        return JsonResponse({"message": f"Got user's email {user_email}", "data": user_id})
