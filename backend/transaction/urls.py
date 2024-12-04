5ca4209cdc115c977e56d1728af1608a99928d30
YoyoLiuuu <yoyoyimeng.liu@mail.utoronto.ca>
2024-12-03 22:27:43 -0500

    replace with abstract class

--- backend/transaction/urls.py
+++ backend/transaction/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import TransactionView
-from .use_case import TransactionUseCase
+from .abstract_use_case import AbstractTransactionUseCase
 from .calculations import Calculations
 from utils.firebase_data_access_implementation import FirebaseDataAccess
 
-transaction_view = TransactionView(TransactionUseCase(Calculations(), FirebaseDataAccess()))
+transaction_view = TransactionView(AbstractTransactionUseCase(Calculations(), FirebaseDataAccess()))
 
 urlpatterns = [
     path("upload/", transaction_view.upload_data_to_firestore, name="upload_data"),
